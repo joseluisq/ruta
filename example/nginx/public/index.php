1dddcc8b0e4cf85b26086e2f303681e432131662
Jose Quintana <joseluisquintana20@gmail.com>
2021-05-12 00:25:09 +0200

    refactor: update example

--- example/nginx/public/index.php
+++ example/nginx/public/index.php
@@ -1,51 +1,41 @@
 <?php
-
 // Routing examples
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require '../../../src/Ruta.php';
 
-Ruta::get('/home/hola', function (Request $req, Response $resp, array $args) {
-    // echo "<pre>";
-    // echo "<b>GET /home/hola</b>\n";
-    // var_dump($args);
-    // echo "</pre>\n";
-
-    $resp->json(['data' => 'hello world!']);
+// 1. Using callbacks
+Ruta::get('/home/hola', function (Request $req, Response $res, array $args) {
+    $res->json(['data' => 'hello world!']);
+});
+Ruta::put('/home/hola', function (Request $req, Response $res, array $args) {
+    $res->redirect('/home/aaa/some/bbb');
+});
+Ruta::post('/home/{path}', function (Request $req, Response $res, array $args) {
+    $res
+        ->status(200)
+        ->header('X-Header-One', 'Header Value 1')
+        ->header('X-Header-Two', 'Header Value 2')
+        ->json(['some_data' => 223424234]);
 });
 
-Ruta::put('/home/hola', function (Request $req, Response $resp, array $args) {
-    // echo "<pre>";
-    // echo "<b>POST /home/hola</b>\n";
-    // var_dump($args);
-    // echo "</pre>\n";
-
-    $resp->redirect('/home/aaa/some/bbb');
-});
-
-Ruta::put('/home/{path}', function (Request $req, Response $resp, array $args) {
-    // echo "<pre>";
-    // echo "<b>PUT /home/{path}</b>\n";
-    // var_dump('PATH: ' . $args['path']);
-    // var_dump($req->multipart());
-    // var_dump($req->query());
-    // echo "</pre>\n";
-
-    $resp
-        ->header('X-Header-One', 'Header Value')
-        ->header('X-Header-Two', 'Header Value')
-        ->json(['data' => 123123123]);
-});
-
+// 2. Using a class and method
 class HomeCtrl {
-    public function index(Request $req, Response $resp, array $args) {
-        echo "<pre>";
-        echo "<b>GET /home/{path1}/some/{path2}</b>\n";
-        var_dump($args);
-        echo "</pre>\n";
+    public function index(Request $req, Response $res, array $args) {
+        // 1.1 $args contains route placeholder values
+        if ($args['path1']) { }
+        // 1.2. Get data provided via `multipart/form-data` 
+        $data = $req->multipart();
+        // 1.3. Get data provided via `application/x-www-form-urlencoded` 
+        $data = $req->urlencoded();
+        // 1.4. Get data provided via `application/json`
+        $data = $req->json();
+        // 1.5. Get data provided via `application/xml`
+        $data = $req->xml();
+        // 1.6. Get query data
+        $data = $req->query();
+        $res->json(['data' => 'hello world!']);
     }
 }
-
 Ruta::get('/home/{path1}/some/{path2}', [HomeCtrl::class, 'index']);
